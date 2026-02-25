6b2833cbe7639828c54fb869db8826022184ccc0
Javid Mamadli <javid.mammadli99@gmail.com>
2026-02-25 19:01:07 +0400

    dadsa

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -65,15 +65,15 @@ const casinosRawData: CasinoRawData[] = [
     votes: 3986,
     isMobile: true
   },
-  // {
-  //   name: "Lizaro",
-  //   logo: "/lizaro.png",
-  //   // bonus: "350% up to £680 + 200 FREE SPINS",
-  //   bonus: "200% up to £1000 + 250 FREE SPINS",
-  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=lizaro",
-  //   votes: 5872,
-  //   isMobile: true
-  // },
+  {
+    name: "Lizaro",
+    logo: "/lizaro.png",
+    // bonus: "350% up to £680 + 200 FREE SPINS",
+    bonus: "200% up to £1000 + 250 FREE SPINS",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=lizaro",
+    votes: 5872,
+    isMobile: true
+  },
   // {
   //   name: "SpinFin",
   //   logo: "/spinfin.svg",
