4abfe3e22286b598ef48b2d35a23974e309098e2
Javid Mamadli <javid.mammadli99@gmail.com>
2026-03-02 00:45:40 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -33,20 +33,20 @@ const topBadges = ["Best Bonus", "Top Pick", "Trending Now"];
 // Raw casino data without id, rating, and badgessss
 const casinosRawData: CasinoRawData[] = [
   {
+    name: "SpinShark",
+    logo: "/spinshark.svg",
+    bonus: "£900 BONUS + 150 Free Spins",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
+    votes: 4143,
+    isMobile: true
+  },
+  {
     name: "Lizaro",
     logo: "/lizaro.png",
     // bonus: "350% up to £680 + 200 FREE SPINS",
     bonus: "200% up to £1000 + 250 FREE SPINS",
     url: "https://topbritcasinos.com/B7W2b6jr?utm_target=lizaro",
     votes: 5872,
-    isMobile: true
-  },
-  {
-    name: "SpinShark",
-    logo: "/spinshark.svg",
-    bonus: "£900 BONUS + 150 Free Spins",
-    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
-    votes: 4143,
     isMobile: true
   },
   {
